07df1f0a0b2060782dc85bc206157daf0cf52fad
Mohammad Afandy <afandy9895@gmail.com>
2021-01-16 18:01:50 +0700

    - enable cors from all origin

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,15 +1,17 @@
 const cors = require("cors");
-const { allowedOrigin } = appRequire("config");
+// const { allowedOrigin } = appRequire("config");
 
-const corsMiddleware = cors({
-  origin: (origin, callback) => {
-    const isAllowed = allowedOrigin.some(v => new RegExp(v, "i").test(origin));
-    if (isAllowed) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not Allowed By CORS"));
-    }
-  }
-});
+// const corsMiddleware = cors({
+//   origin: (origin, callback) => {
+//     const isAllowed = allowedOrigin.some(v => new RegExp(v, "i").test(origin));
+//     if (isAllowed) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Your IP Doesn't"));
+//     }
+//   }
+// });
+
+const corsMiddleware = cors();
 
 module.exports = corsMiddleware;
